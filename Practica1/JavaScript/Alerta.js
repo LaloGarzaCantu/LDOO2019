9ad982c7b75975db1711f8ce024c0b6f5deb9973
LaloGarzaCantu <lalo.garza.cantu@hotmail.com>
2019-02-23 16:03:58 -0600

    Practica1
    Practica1

---
+++ Practica1/JavaScript/Alerta.js
@@ -0,0 +1,14 @@
+
+<html>
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <script>
+			
+		alert("Bienvenido");
+		</script>
+    </body>
+</html>
